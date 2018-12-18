753ca4c8fe81905ec2fb7d58b011efbfae953d0f
Eric Dill <edill@anaconda.com>
2018-12-18 05:34:29 -0500

    Add skeleton of test harness

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import find_packages, setup
-
+import versioneer
 
 setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     name='livy-submit',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
             'livy-submit = livy_submit.main:cli'
-
         ]
     }
 )
